dbd8b537226a4b39255e3af7a1e2cb30c5966a57
ybibo <2727146286@qq.com>
2023-01-18 19:38:23 +0800

    version 1.0

---
+++ js/apps/main.js
@@ -0,0 +1,140 @@
+// JavaScript Document
+
+$("#error-alert").hide();
+$("#copy-alert").hide();
+
+var password = "喵嗷呜~";
+
+String.prototype.replaceAll = function (s1, s2) {
+	var reg = new RegExp(s1, "g");
+	return this.replace(reg, s2);
+}
+
+function encrypt() {
+	var msg = $("#text-decryped").val();
+	var key = $("#text-key").val();
+
+	if (msg.length < 1) {
+		$("#error-alert").show();
+		$("#copy-alert").hide();
+		$("#error-alert").text("请输入待【嗷呜】加密的明文");
+	} else {
+		if (key.length < 4) {
+			key = password;
+		}
+
+		$("#text-encryped").val(encode(msg,key));
+		$("#error-alert").hide();
+		$("#copy-alert").hide();
+	}
+
+}
+
+function decrypt() {
+	var msg = $("#text-decryped").val();
+	var key = $("#text-key").val();
+
+	if (msg.length < 1) {
+		$("#error-alert").show();
+		$("#copy-alert").hide();
+		$("#error-alert").text("请输入待【喵呜】解密的密文）");
+	}else {
+			if (key.length < 4) {
+				key = password;
+			}
+
+			try {
+				$("#error-alert").hide();
+				var str = decode(msg,key);
+			} catch (err) {
+				$("#error-alert").show();
+				$("#copy-alert").hide();
+				$("#error-alert").text("【嗷呜】有误，请确定【嗷呜】（密文）正确并未被篡改");
+			} finally {
+				$("#text-encryped").val(str);
+
+			}
+
+
+
+		}
+
+
+}
+
+
+function copyUrl2() {
+	var Url2 = document.getElementById("text-encryped");
+	Url2.select();
+	document.execCommand("Copy");
+	$("#copy-alert").show();
+	$("#error-alert").hide();
+}
+
+function encode(rawStr,key) {
+	let charArr = rawStr.split("")
+	let unicodeHexStr = ""
+	let beastDictArr = key.split("")
+
+	for (let i = 0; i < charArr.length; i++) {
+		let charHexStr = charArr[i].charCodeAt(0).toString(16)
+		while (charHexStr.length < 4) {
+			charHexStr = "0" + charHexStr
+		}
+		unicodeHexStr += charHexStr
+	}
+	let k = 0
+	let unicodeHexStrArr = unicodeHexStr.split("")
+	let beastStr = ""
+	for (let i = 0; i < unicodeHexStrArr.length; i++) {
+		let unicodeHexCharValue = parseInt("0x" + unicodeHexStrArr[i])
+		k = unicodeHexCharValue + (i % 0x10)
+		if (k >= 0x10) {
+			k = k - 0x10;
+		}
+		beastStr += beastDictArr[parseInt(k / 4)] + beastDictArr[(k % 4)]
+	}
+	return beastStr
+}
+
+function decode(beastStr,key) {
+	let unicodeHexStr = ""
+	let beastStrArr = beastStr.split("")
+	let beastDictArr = key.split("")
+
+
+	for (let i = 0; i <= (beastStr.length - 2); i += 2) {
+		let beastCharStr = ""
+		let pos1 = 0
+		beastCharStr = beastStrArr[i];
+		for (; pos1 <= 3; pos1++) {
+			if (beastCharStr == beastDictArr[pos1]) {
+				break
+			}
+		}
+		let pos2 = 0
+		beastCharStr = beastStrArr[i + 1]
+		for (; pos2 <= 3; pos2++) {
+			if (beastCharStr == beastDictArr[pos2]) {
+				break;
+			}
+		}
+		let k = (pos1 * 4) + pos2;
+		let unicodeHexCharValue = k - (parseInt(i / 2) % 0x10);
+		if (unicodeHexCharValue < 0) {
+			unicodeHexCharValue += 0x10;
+		}
+		unicodeHexStr += unicodeHexCharValue.toString(16)
+	}
+	let rawStr = ""
+	let start = 0
+	let end = 4
+	while (end <= unicodeHexStr.length) {
+		let charHexStr = unicodeHexStr.substring(start, end);
+		let charStr = String.fromCharCode(parseInt("0x" + charHexStr))
+		rawStr += charStr
+		start += 4
+		end += 4
+	}
+	return rawStr
+}
